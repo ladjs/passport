24292733061dd68a06289d6860f6cf555ae73db7
niftylettuce <niftylettuce@gmail.com>
2019-11-20 13:26:36 -0600

    feat: added passport-github, fixed user setting, bump deps

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const passport = require('koa-passport');
+const GitHubStrategy = require('passport-github').Strategy;
 const _ = require('lodash');
-const { OAuth2Strategy } = require('passport-google-oauth');
 const boolean = require('boolean');
+const passport = require('koa-passport');
+const validator = require('validator');
+const { OAuth2Strategy } = require('passport-google-oauth');
 
 function Passport(Users, config) {
   if (!_.isObject(Users)) throw new Error('Users object not defined');
@@ -24,34 +26,20 @@ function Passport(Users, config) {
     },
     providers: {
       local: boolean(process.env.AUTH_LOCAL_ENABLED),
-      google: boolean(process.env.AUTH_GOOGLE_ENABLED)
-      /*
-      facebook: boolean(process.env.AUTH_FACEBOOK_ENABLED),
-      twitter: boolean(process.env.AUTH_TWITTER_ENABLED),
-      github: boolean(process.env.AUTH_GITHUB_ENABLED),
-      linkedin: boolean(process.env.AUTH_LINKEDIN_ENABLED),
-      instagram: boolean(process.env.AUTH_INSTAGRAM_ENABLED),
-      stripe: boolean(process.env.AUTH_STRIPE_ENABLED)
-      */
+      google: boolean(process.env.AUTH_GOOGLE_ENABLED),
+      github: boolean(process.env.AUTH_GITHUB_ENABLED)
     },
     strategies: {
       google: {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: process.env.GOOGLE_CALLBACK_URL
-      }
-      /*
-      facebook: {
-        clientID: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: process.env.FACEBOOK_CALLBACK_URL
       },
-      twitter: {},
-      github: {},
-      linkedin: {},
-      instagram: {},
-      stripe: {}
-      */
+      github: {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK_URL
+      }
     },
     google: {
       accessType: 'offline',
@@ -61,6 +49,9 @@ function Passport(Users, config) {
         'https://www.googleapis.com/auth/userinfo.profile'
       ]
     },
+    github: {
+      scope: ['user:email']
+    },
     fields: {
       displayName: 'display_name',
       givenName: 'given_name',
@@ -68,7 +59,10 @@ function Passport(Users, config) {
       avatarURL: 'avatar_url',
       googleProfileID: 'google_profile_id',
       googleAccessToken: 'google_access_token',
-      googleRefreshToken: 'google_refresh_token'
+      googleRefreshToken: 'google_refresh_token',
+      githubProfileID: 'github_profile_id',
+      githubAccessToken: 'github_access_token',
+      githubRefreshToken: 'github_refresh_token'
     }
   });
 
@@ -80,50 +74,84 @@ function Passport(Users, config) {
   if (config.providers.local && _.isFunction(Users.createStrategy))
     passport.use(Users.createStrategy());
 
+  if (config.providers.github)
+    passport.use(
+      new GitHubStrategy(
+        config.strategies.github,
+        async (accessToken, refreshToken, profile, done) => {
+          try {
+            const email = profile.emails[0].value;
+
+            let user = await Users.findOne({ email });
+            if (!user) user = new Users({ email });
+            ['displayName', 'givenName', 'familyName'].forEach(key => {
+              if (!user[fields[key]] && profile[key])
+                user[fields[key]] = profile[key];
+            });
+
+            user[fields.githubProfileID] = profile.id;
+            user[fields.githubAccessToken] = accessToken;
+            user[fields.githubRefreshToken] = refreshToken;
+
+            if (
+              (!_.isString(user[fields.avatarURL]) ||
+                !validator.isURL(user[fields.avatarURL])) &&
+              _.isArray(profile.photos) &&
+              !_.isEmpty(profile.photos) &&
+              _.isObject(profile.photos[0]) &&
+              _.isString(profile.photos[0].value) &&
+              validator.isURL(profile.photos[0].value)
+            )
+              user[fields.avatarURL] = profile.photos[0].value;
+
+            await user.save();
+
+            done(null, user.toObject());
+          } catch (err) {
+            done(err);
+          }
+        }
+      )
+    );
+
   if (config.providers.google)
     passport.use(
       new OAuth2Strategy(
         config.strategies.google,
         async (accessToken, refreshToken, profile, done) => {
-          const email = profile.emails[0].value;
-
           try {
+            const email = profile.emails[0].value;
+
             let user = await Users.findOne({ email });
+            if (!user) user = new Users({ email });
+            ['displayName', 'givenName', 'familyName'].forEach(key => {
+              if (!user[fields[key]] && profile[key])
+                user[fields[key]] = profile[key];
+            });
 
-            if (user) {
-              // store the access token and refresh token
-              if (accessToken) user.set(fields.googleAccessToken, accessToken);
-              if (refreshToken)
-                user.set(fields.googleRefreshToken, refreshToken);
-              user = await user.save();
-            } else {
-              // there is still a bug that doesn't let us revoke tokens
-              // in order for us to get a new refresh token per:
-              // <http://stackoverflow.com/a/18578660>
-              // so instead we explicitly send them to the google url
-              // with `prompt=consent` specified (this rarely happens)
-              if (!refreshToken) return done(new Error('Consent required'));
+            user[fields.googleProfileID] = profile.id;
+            user[fields.googleAccessToken] = accessToken;
+            user[fields.googleRefreshToken] = refreshToken;
 
-              const obj = { email };
-              obj[fields.displayName] = profile.displayName;
-              obj[fields.givenName] = profile.name.givenName;
-              obj[fields.familyName] = profile.name.familyName;
-              obj[fields.googleProfileID] = profile.id;
-              obj[fields.googleAccessToken] = accessToken;
-              obj[fields.googleRefreshToken] = refreshToken;
-
-              if (
-                _.isObject(profile._json.image) &&
-                _.isString(profile._json.image.url)
-              ) {
-                // we don't want ?sz= in the image URL
-                obj[fields.avatarURL] = profile._json.image.url.split(
-                  '?sz='
-                )[0];
-              }
-
-              user = await Users.create(obj);
+            if (
+              (!_.isString(user[fields.avatarURL]) ||
+                !validator.isURL(user[fields.avatarURL])) &&
+              _.isObject(profile._json.image) &&
+              _.isString(profile._json.image.url) &&
+              validator.isURL(profile._json.image.url)
+            ) {
+              // we don't want ?sz= in the image URL
+              user[fields.avatarURL] = profile._json.image.url.split('?sz=')[0];
             }
+
+            await user.save();
+
+            // there is still a bug that doesn't let us revoke tokens
+            // in order for us to get a new refresh token per:
+            // <http://stackoverflow.com/a/18578660>
+            // so instead we explicitly send them to the google url
+            // with `prompt=consent` specified (this rarely happens)
+            if (!refreshToken) return done(new Error('Consent required'));
 
             done(null, user.toObject());
           } catch (err) {
